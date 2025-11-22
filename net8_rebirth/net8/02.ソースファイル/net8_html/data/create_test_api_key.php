4f9e81a2839c7e6f8bb7192205f44e7ffafdf47e
Voice Suite User <user@example.com>
2025-11-22 18:56:31 +0900

    fix: create_test_api_key.phpのrequire_filesパスを修正

--- net8_rebirth/net8/02.ソースファイル/net8_html/data/create_test_api_key.php
+++ net8_rebirth/net8/02.ソースファイル/net8_html/data/create_test_api_key.php
@@ -6,7 +6,7 @@
 
 header('Content-Type: application/json');
 
-require_once(__DIR__ . '/_etc/require_files.php');
+require_once(__DIR__ . '/../_etc/require_files.php');
 
 try {
     $pdo = get_db_connection();
